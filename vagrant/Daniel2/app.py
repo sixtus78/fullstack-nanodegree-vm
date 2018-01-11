c3448ee3bfc1f093a9635b930b7e136aacf3446e
Daniel Sixl <sixl@sixl.org>
2018-01-11 22:09:07 +0100

    API endpoints (JSON) added

--- vagrant/Daniel2/app.py
+++ vagrant/Daniel2/app.py
@@ -6,11 +6,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 from datetime import datetime
 from flask import Flask
-from flask import render_template, url_for, request, redirect, flash
+from flask import render_template, url_for, request, redirect, flash, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://web4_10:lwe7derUcmXU5MIT3u1Z@https://s65.goserver.host:3306/web4_db10'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
@@ -29,6 +28,12 @@ class Restaurant(Base):
     menu = db.relationship('MenuItem', backref='restaurant', lazy=False)
     def __repr__(self):
         return self.name
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'name' : self.name
+        }
 
 # Model for MenuItem
 class MenuItem(Base):
@@ -39,6 +44,16 @@ class MenuItem(Base):
     course = db.Column(db.String(800))
     def __repr__(self):
         return self.name
+    @property
+    def serialize(self):
+        return {
+            'id' : self.id,
+            'restaurant_id' : self.restaurant_id,
+            'name' : self.name,
+            'price' : self.price,
+            'description' : self.description,
+            'course' : self.course,
+        }
 
 now = datetime.now()
 currentYear = now.year
@@ -49,29 +64,27 @@ def inject_year():
 #Fake Restaurants
 #restaurant = {'name': 'The CRUDdy Crab', 'id': '1'}
 #restaurants = [{'name': 'The CRUDdy Crab', 'id': '1'}, {'name':'Blue Burgers', 'id':'2'}, {'name':'Taco Hut', 'id':'3'}]
-#restaurants = []
 
 #Fake Menu Items
 #items = [ {'name':'Cheese Pizza', 'description':'made with fresh cheese', 'price':'$5.99','course' :'Entree', 'id':'1'}, {'name':'Chocolate Cake','description':'made with Dutch Chocolate', 'price':'$3.99', 'course':'Dessert','id':'2'},{'name':'Caesar Salad', 'description':'with fresh organic vegetables','price':'$5.99', 'course':'Entree','id':'3'},{'name':'Iced Tea', 'description':'with lemon','price':'$0.99', 'course':'Beverage','id':'4'},{'name':'Spinach Dip', 'description':'creamy dip with fresh spinach','price':'$1.99', 'course':'Appetizer','id':'5'} ]
 #item =  {'name':'Cheese Pizza','description':'made with fresh cheese','price':'$5.99','course' :'Entree'}
-#items = []
 
 ### INDEX / SHOW ALL RESTAURANTS
 
-@app.route('/restaurants')
+@app.route('/restaurants/')
 @app.route('/')
 def showAllRestaurants():
     restaurants = Restaurant.query.all()
     return render_template('restaurant/showAllRestaurants.html',restaurants=restaurants)
 
-@app.route('/test')
-@app.route('/t')
+@app.route('/test/')
+@app.route('/t/')
 def showTestPage():
     return render_template('test.html')
 
 ### RESTAURANTS
 
-@app.route('/restaurant/new', methods=['GET', 'POST'])
+@app.route('/restaurant/new/', methods=['GET', 'POST'])
 def createRestaurant():
     if request.method == 'POST':
         newRestaurant = Restaurant (
@@ -84,7 +97,7 @@ def createRestaurant():
     else:
         return render_template('restaurant/createRestaurant.html')
 
-@app.route('/restaurant/<int:restaurant_id>/edit', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/edit/', methods=['GET', 'POST'])
 def editRestaurant(restaurant_id):
     restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
     if request.method == 'POST':
@@ -96,7 +109,7 @@ def editRestaurant(restaurant_id):
     else:
         return render_template('restaurant/editRestaurant.html',restaurant=restaurant)
 
-@app.route('/restaurant/<int:restaurant_id>/delete', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/delete/', methods=['GET', 'POST'])
 def deleteRestaurant(restaurant_id):
     restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
     if request.method == 'POST':
@@ -109,14 +122,14 @@ def deleteRestaurant(restaurant_id):
 
 ### MENUS
 
-@app.route('/<int:restaurant_id>')
-@app.route('/<int:restaurant_id>/menu')
+@app.route('/<int:restaurant_id>/')
+@app.route('/<int:restaurant_id>/menu/')
 def showMenu(restaurant_id):
     restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
     items = restaurant.menu
     return render_template('menu/showMenu.html',restaurant=restaurant,items=items)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/new', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/new/', methods=['GET', 'POST'])
 def createMenuItem(restaurant_id):
     restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
     if request.method == 'POST':
@@ -134,7 +147,7 @@ def createMenuItem(restaurant_id):
     else:
         return render_template('menu/newMenuItem.html',restaurant=restaurant)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id,menu_id):
     item = MenuItem.query.filter_by(id=menu_id).one()
     restaurant = item.restaurant
@@ -150,7 +163,7 @@ def editMenuItem(restaurant_id,menu_id):
     else:
         return render_template('menu/editMenuItem.html',restaurant=restaurant,item=item)
 
-@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete', methods=['GET', 'POST'])
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/delete/', methods=['GET', 'POST'])
 def deleteMenuItem(restaurant_id,menu_id):
     item = MenuItem.query.filter_by(id=menu_id).one()
     restaurant = item.restaurant
@@ -161,6 +174,23 @@ def deleteMenuItem(restaurant_id,menu_id):
         return redirect(url_for('showMenu',restaurant_id=restaurant_id))
     else:
         return render_template('menu/deleteMenuItem.html',restaurant=restaurant,item=item)
+
+### API Endpoints (GET Request)
+
+@app.route('/restaurants/json/')
+def restaurantsAllJSON():
+    restaurants = Restaurant.query.all()
+    return jsonify(Restaurants=[r.serialize for r in restaurants])
+
+@app.route('/restaurant/<int:restaurant_id>/menu/json/')
+def restaurantJSON(restaurant_id):
+    restaurant = Restaurant.query.filter_by(id=restaurant_id).one()
+    return jsonify(restaurant.serialize)
+
+@app.route('/restaurant/<int:restaurant_id>/menu/<int:menu_id>/json/')
+def restaurantMenuJSON(restaurant_id, menu_id):
+    menu = MenuItem.query.filter_by(id=menu_id).one()
+    return jsonify(menu.serialize)
 
 ### ERROR CODES
 
